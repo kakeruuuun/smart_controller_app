1392764f3bb73faafe79bfb8cb2b485e3aa17f1f
かけるん <kakeru213.niconico@gmail.com>
2017-12-25 16:35:11 +0900

    ベース作成
    OAuth認証からアプリに戻ってくるまでを実装

--- app/src/main/java/com/example/tsuchiyashouhei/smart_controller/MainActivity.java
+++ app/src/main/java/com/example/tsuchiyashouhei/smart_controller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tsuchiyashouhei.smart_controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,18 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (!TwitterUtils.hasAccessToken(this)) {
+            Intent intent = new Intent(this, TwitterOAuthActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
