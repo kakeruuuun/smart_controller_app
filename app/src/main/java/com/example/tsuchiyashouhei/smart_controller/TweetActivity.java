a8e8d1920d6c7c42d30207850b091ab46d06e7fa
かけるん <kakeru213.niconico@gmail.com>
2017-12-29 15:28:26 +0900

    クラス追加
    Activityを追加。マニフェストも変更。

---
+++ app/src/main/java/com/example/tsuchiyashouhei/smart_controller/TweetActivity.java
@@ -0,0 +1,13 @@
+package com.example.tsuchiyashouhei.smart_controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TweetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet);
+    }
+}
